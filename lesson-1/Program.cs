6daef1236a985f10e154277ff1bd9d99f27e41d3
Kirill Feshchenko <57315983+suicidetoday@users.noreply.github.com>
2020-01-20 20:50:49 +0300

    ⭐️Upload program.cs

---
+++ lesson-1/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Threading;
+
+namespace homework_1
+{
+    class Program
+    {
+        static int score = 0;
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Привет! Это опрос на знание терминов по информатике.");
+            Console.ResetColor();
+            Thread.Sleep(3000);
+            Console.Clear();
+            template("Что из представленного ниже не является яп?", "1 - html, css \r\n2 - cpp, ruby\r\n3 - python, rust", "1");
+            template("Какие значения принимает int?", "1 - строковые \r\n2 - числовые\r\n3 - разные\r\n4 - всякие ", "2");
+            template("Что такое табуляция?", "1 - пробел \r\n2 - ascii символ для выравнивания текста\r\n3 - язык программирования", "2");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine($"Конец! Правильных ответов - {score}.");
+            Console.ResetColor();
+        }
+        
+        static void template(string questionText, string questionVariables, string correctAnswer)
+        {
+            Console.WriteLine("–––––––––––––––––––");
+            Console.WriteLine(questionText);
+            Console.WriteLine(questionVariables);
+            Console.WriteLine("–––––––––––––––––––");
+            Console.WriteLine("Ответ:");
+            string answer = Console.ReadLine();
+            if (correctAnswer == answer)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Правильно!");
+                ++score;
+            }
+            else
+            {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine($"Неправильно! Правильный ответ – {correctAnswer}.");
+            }
+            Thread.Sleep(3000);
+            Console.Clear();
+            Console.ResetColor();
+        }
+    }
+}
+
+
